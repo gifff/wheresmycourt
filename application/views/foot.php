85de69290f6413d536427c59b06d17446d45a343
Gifary <gifarydhimas@gmail.com>
2017-03-10 21:59:13 +0700

    separation head and foot from the original view, the welcome.php

--- application/views/foot.php
+++ application/views/foot.php
@@ -1,17 +1,28 @@
-        </div>
-        <!-- /#page-content-wrapper -->
-
-    </div>
-    <!-- /#wrapper -->
-
-    <!-- Menu Toggle Script -->
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
-
-</body>
-
+        <nav id="tf-footer">
+            <div class="container">
+                <div class="pull-left">
+                    <p>2015 © Awesomess. All Rights Reserved. Designed and Coded by <a href="https://dribbble.com/jennpereira">Jenn</a> for <a href="https://themeforces.com">ThemeForces.Com</a></p>
+                </div>
+                <div class="pull-right">
+                    <ul class="social-media list-inline">
+                        <li><a href="#"><span class="fa fa-facebook"></span></a></li>
+                        <li><a href="#"><span class="fa fa-twitter"></span></a></li>
+                        <li><a href="#"><span class="fa fa-pinterest"></span></a></li>
+                        <li><a href="#"><span class="fa fa-google-plus"></span></a></li>
+                        <li><a href="#"><span class="fa fa-dribbble"></span></a></li>
+                        <li><a href="#"><span class="fa fa-behance"></span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="<?= base_url('HTML/Skuy/text/javascript" src="js/jquery.1.11.1.js'); ?>"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script type="text/javascript" src="<?= base_url('HTML/Skuy/js/bootstrap.js'); ?>"></script>
+        <!-- Javascripts
+        ================================================== -->
+        <script type="text/javascript" src="<?= base_url('HTML/Skuy/js/main.js'); ?>"></script>
+    </body>
 </html>
--- application/views/head.php
+++ application/views/head.php
@@ -1,48 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="ekohort">
-    <meta name="author" content="Gifary Dhimas Fadhillah">
-
-    <title><?= $this->config->item('website_name', 'tank_auth');?></title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="<?= base_url("/public/css/bootstrap.min.css"); ?>" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="<?= base_url("/public/css/simple-sidebar.css"); ?>" rel="stylesheet">
-
-	<script src="<?= base_url("/public/js/jquery.min.js"); ?>"></script>
-	<script src="<?= base_url("/public/js/bootstrap.min.js"); ?>"></script>
-  <?php if(isset($user_id) && isset($username)): ?>
-  <script>
-    var user_id = <?= $user_id; ?>, username = '<?= $username; ?>';
-  </script>
-<?php endif; ?>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-<body>
-<!-- Navigation --/>
-	<nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
-		<!-- Brand and toggle get grouped for better mobile display --/>
-		<div class="navbar-header">
-			<button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-		</div>
-	</nav>
-<!-->
+    <head>
+        <!-- Basic Page Needs
+        ================================================== -->
+        <meta charset="utf-8">
+        <!--[if IE]><meta http-equiv="x-ua-compatible" content="IE=9" /><![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Awesomess - Portfolio Bootstrap Theme</title>
+        <meta name="description" content="Your Description Here">
+        <meta name="keywords" content="bootstrap themes, portfolio, responsive theme">
+        <meta name="author" content="ThemeForces.Com">
+        
+        <!-- Favicons
+        ================================================== -->
+        <link rel="shortcut icon" href="<?= base_url('HTML/Skuy/img/favicon.ico'); ?>" type="image/x-icon">
+        <link rel="apple-touch-icon" href="<?= base_url('HTML/Skuy/img/apple-touch-icon.png'); ?>">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url('HTML/Skuy/img/apple-touch-icon-72x72.png'); ?>">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url('HTML/Skuy/img/apple-touch-icon-114x114.png'); ?>">
+        <!-- Bootstrap -->
+        <link rel="stylesheet" type="text/css"  href="<?= base_url('HTML/Skuy/css/bootstrap.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?= base_url('HTML/Skuy/fonts/font-awesome/css/font-awesome.css'); ?>">
+        <!-- Stylesheet
+        ================================================== -->
+        <link rel="stylesheet" type="text/css"  href="<?= base_url('HTML/Skuy/css/style.css'); ?>">
+        <link rel="stylesheet" type="text/css" href="<?= base_url('HTML/Skuy/css/responsive.css'); ?>">
+        <script type="text/javascript" src="<?= base_url('HTML/Skuy/js/modernizr.custom.js'); ?>"></script>
+        <link href='http://fonts.googleapis.com/css?family=Raleway:500,600,700,100,800,900,400,200,300' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div id="tf-home">
+            <div class="overlay">
+                <div id="sticky-anchor"></div>
+                <nav id="tf-menu" class="navbar navbar-default">
+                    <div class="container">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                            <a class="navbar-brand logo" href="index.html">S-kuy!</a>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#tf-home">Home</a></li>
+                                <li><a href="#tf-service">About</a></li>
+                                <li><a href="#tf-about">News</a></li>
+                                <li><a href="#tf-contact">Contact</a></li>
+                            </ul>
+                            </div><!-- /.navbar-collapse -->
+                            </div><!-- /.container-fluid -->
+                        </nav>
