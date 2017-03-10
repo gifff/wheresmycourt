83653b78b47818236e8a3e8c130a0297619bd72b
Gifary <gifarydhimas@gmail.com>
2017-03-10 23:15:37 +0700

    init after login view

---
+++ application/views/pilih_lapangan.php
@@ -0,0 +1,103 @@
+            <div id="tf-service">
+              <div class="container">
+                <div class="col-md-4">
+                  <img src="<?= base_url('HTML/Skuy/img/futsal.png'); ?>" style="margin-left:100px; height:165px; width:165px"/>
+                  
+                  <div class="media">
+                    <div class="media-left media-middle">
+                      
+                      
+                      
+                    </div>
+                    <div class="media-body">
+                      <a href="#tf-contact" class="btn btn-primary my-btna">Booking</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4">
+                  
+                  <img src="<?= base_url('HTML/Skuy/img/badminton.png'); ?>" style="margin-left:100px; height:165px; width:165px"/>
+                  
+                  
+                  <div class="media">
+                    <div class="media-left media-middle">
+                      
+                    </div>
+                    <div class="media-body">
+                      <a href="#tf-contact" class="btn btn-primary my-btna">Booking</a>
+                      
+                    </div>
+                  </div>
+                  
+                </div>
+                <div class="col-md-4">
+                  
+                  <img src="<?= base_url('HTML/Skuy/img/basket.png'); ?>" style="margin-left:100px; height:165px; width:165px"/>
+                  
+                  <div class="media">
+                    <div class="media-left media-middle">
+                      
+                    </div>
+                    <div class="media-body">
+                      <a href="#tf-contact" class="btn btn-primary my-btna">Booking</a>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+            <div id="tf-portfolio">
+              <div class="container">
+                <div class="section-title">
+                  <h3>My Latest Works</h3>
+                  <hr>
+                </div>
+                <div class="space"></div>
+                <div id="tf-about">
+                  <div class="overlay">
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-md-6 col-md-offset-6">
+                          <h3>News</h3>
+                          <br>
+                          <p>Cras sit amet nibh libero, in gravida nulla. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                          <p>Metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                          <br>
+                          <a href="#tf-why-me" class="btn btn-primary my-btn dark">Why Hire Me</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div id="tf-contact">
+              <div class="container">
+                <div class="section-title">
+                  <h3>Contact Me</h3>
+                  <p>Cras sit amet nibh libero, in gravida nulla. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.</p>
+                  <hr>
+                </div>
+                <div class="space"></div>
+                <div class="row">
+                  <div class="col-md-6 col-md-offset-3">
+                    <form id="contact">
+                      <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Name">
+                      </div>
+                      <div class="form-group">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Email">
+                      </div>
+                      <div class="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Website">
+                      </div>
+                      <div class="form-group">
+                        <textarea class="form-control" rows="4" placeholder="Message"></textarea>
+                      </div>
+                      <button type="submit" class="btn btn-primary my-btn dark">Submit</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
